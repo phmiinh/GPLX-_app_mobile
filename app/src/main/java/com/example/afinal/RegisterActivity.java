f51a399d47ef776ac9bd33c6dad38dd59fcda222
phatnguyen0234 <151380074+phatnguyen0234@users.noreply.github.com>
2025-10-24 12:35:55 +0700

    skiplogin

--- app/src/main/java/com/example/afinal/RegisterActivity.java
+++ app/src/main/java/com/example/afinal/RegisterActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth; // <-- Thêm thư viện Firebase Auth
 
 public class RegisterActivity extends AppCompatActivity {
@@ -19,6 +20,11 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputEditText etUsername, etEmail, etPassword, etConfirmPassword;
     private Button btnRegister;
     private TextView tvLoginNow;
+
+    private TextInputLayout layoutUsername;
+    private TextInputLayout layoutEmail;
+    private TextInputLayout layoutPassword;
+    private TextInputLayout layoutConfirmPassword;
 
     private FirebaseAuth mAuth;
 
@@ -35,6 +41,10 @@ public class RegisterActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.et_email_register);
         etPassword = findViewById(R.id.et_password_register);
         etConfirmPassword = findViewById(R.id.et_confirm_password_register);
+        layoutUsername = findViewById(R.id.layout_username);
+        layoutEmail = findViewById(R.id.layout_email);
+        layoutPassword = findViewById(R.id.layout_password);
+        layoutConfirmPassword = findViewById(R.id.layout_confirm_password);
         btnRegister = findViewById(R.id.btn_register);
         tvLoginNow = findViewById(R.id.tv_login_now);
 
@@ -50,43 +60,49 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void handleRegister() {
+        layoutUsername.setError(null);
+        layoutEmail.setError(null);
+        layoutPassword.setError(null);
+        layoutConfirmPassword.setError(null);
         String username = etUsername.getText().toString().trim();
         String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
         String confirmPassword = etConfirmPassword.getText().toString().trim();
 
         if (TextUtils.isEmpty(username)) {
-            etUsername.setError("Tên đăng nhập không được để trống");
+            layoutUsername.setError("Tên đăng nhập không được để trống");
             etUsername.requestFocus();
             return;
         }
         if (TextUtils.isEmpty(email)) {
-            etEmail.setError("Email không được để trống");
+            layoutEmail.setError("Email không được để trống");
             etEmail.requestFocus();
             return;
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            etEmail.setError("Vui lòng nhập email hợp lệ");
+            layoutEmail.setError("Vui lòng nhập email hợp lệ");
             etEmail.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(password)) {
-            etPassword.setError("Mật khẩu không được để trống");
-            etPassword.requestFocus();
-            return;
-        }
-        if (password.length() < 6) {
-            etPassword.setError("Mật khẩu phải có ít nhất 6 ký tự");
+        if (TextUtils.isEmpty(password) || !isValidPassword(password)) {
+            String message = "";
+            if(TextUtils.isEmpty(password)) {
+                message = "Mật khẩu không được để trống";
+            }
+            if(!isValidPassword(password)){
+                message = "Mật khẩu phải có ít nhất 6 ký tự, bao gồm chữ cái, số và ký tự đặc biệt";
+            }
+            layoutPassword.setError(message);
             etPassword.requestFocus();
             return;
         }
         if (TextUtils.isEmpty(confirmPassword)) {
-            etConfirmPassword.setError("Vui lòng xác nhận mật khẩu");
+            layoutConfirmPassword.setError("Vui lòng xác nhận mật khẩu");
             etConfirmPassword.requestFocus();
             return;
         }
         if (!password.equals(confirmPassword)) {
-            etConfirmPassword.setError("Mật khẩu không khớp");
+            layoutConfirmPassword.setError("Mật khẩu không khớp");
             etConfirmPassword.requestFocus();
             return;
         }
@@ -105,4 +121,27 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 });
     }
-}
+
+    private boolean isValidPassword(String password) {
+        if (password.length() < 6) {
+            return false;
+        }
+
+        Boolean hasLetter = false;
+        Boolean hasDigit = false;
+        Boolean hasSpecialChar = false;
+
+        for(char c : password.toCharArray()) {
+            if(Character.isLetter(c)) {
+                hasLetter = true;
+            } else if(Character.isDigit(c)) {
+                hasDigit = true;
+            } else hasSpecialChar = true;
+
+            if(hasLetter && hasDigit && hasSpecialChar) {
+                return true;
+            }
+        }
+        return false;
+        }
+    }
